4cf564eed178541b32e7aaba498bfb49c45f09d3
machy96 <65493963+machy96@users.noreply.github.com>
2024-02-09 23:43:00 +0100

    Add files via upload

--- TA/models/email.py
+++ TA/models/email.py
@@ -1,7 +1,9 @@
 from odoo import models, fields, api
 
+
 class Email(models.Model):
     _name = 'ta.email'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = 'E-mail Entrant'
 
     subject = fields.Char(string='Sujet')
@@ -11,9 +13,3 @@ class Email(models.Model):
     received_date = fields.Datetime(string='Date de Réception')
     numero_dum_extracted = fields.Char(string="Numéro DUM Extrait")
     dossier_id = fields.Many2one('ta.dossier', string='Dossier Associé')
-
-    @api.model
-    def create(self, vals):
-        # Ici, vous pouvez ajouter la logique pour extraire le numéro DUM du corps de l'email si nécessaire
-        # et mettre à jour `vals` en conséquence avant de créer l'enregistrement.
-        return super(Email, self).create(vals)
